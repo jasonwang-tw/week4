64e78beff3a24c60b55cb23022a7a68125e65214
JasonWang <ff1986755087@gmail.com>
2020-05-06 21:54:35 +0800

    使用ejs/scss

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const pug = require('gulp-pug'); // 載入 gulp-pug 套件
-
-gulp.task('pug', () => {
-  return gulp
-    .src('views/*.pug') // Pug 主檔案路徑
-    .pipe(pug()) // 使用 gulp-pug 進行編譯
-    .pipe(gulp.dest('public/')); // 編譯完成輸出路徑
-});
